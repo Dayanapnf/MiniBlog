6d69cf498bbf791593ac1f95d666f4c0a28045d7
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-19 18:59:08 -0300

    css navbar

--- MiniBlog/src/components/Footer.jsx
+++ MiniBlog/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import styles from './Footer.module.css';
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva sobre o que tem interesse!</h3>
       <p>Mini blog &copy; 2024</p>
     </footer>
--- MiniBlog/src/components/NavBar.jsx
+++ MiniBlog/src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 const NavBar = () => {
   return (
-    <nav>
-      <NavLink to="/">
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
         Mini <span>Blog</span>
       </NavLink>
-      <ul>
+      <ul className={styles.links_list}>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
