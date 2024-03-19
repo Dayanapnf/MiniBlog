825e60e230d3b36647adeba810b4a2f709389b71
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-19 17:23:07 -0300

    firebase

--- MiniBlog/src/App.jsx
+++ MiniBlog/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ MiniBlog/src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD4OCxxTXTBAjeI1G8D3MTDF2USR1qj3xY',
+  authDomain: 'miniblog-d6357.firebaseapp.com',
+  projectId: 'miniblog-d6357',
+  storageBucket: 'miniblog-d6357.appspot.com',
+  messagingSenderId: '425440055473',
+  appId: '1:425440055473:web:4429d3756b6ac096a09290',
+  measurementId: 'G-0TXJLKVHXB',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { db };
