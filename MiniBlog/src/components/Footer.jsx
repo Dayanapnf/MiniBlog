0b3d65660674a6ee6c4052354371029e39b707cb
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-19 17:22:48 -0300

    footer

---
+++ MiniBlog/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Escreva sobre o que tem interesse!</h3>
+      <p>Mini blog &copy; 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
