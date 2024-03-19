9f1ca9ee1eb0aa63091672d6cb9700c63bca0116
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-19 17:22:55 -0300

    about

---
+++ MiniBlog/src/pages/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//CSS
+import styles from './Aboute.module.css';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
