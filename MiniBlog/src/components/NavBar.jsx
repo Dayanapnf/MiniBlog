47d64697a22c8a51b871b9b3834e99e3a6d09501
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-20 08:52:47 -0300

    css header

--- MiniBlog/src/components/NavBar.jsx
+++ MiniBlog/src/components/NavBar.jsx
@@ -9,10 +9,20 @@ const NavBar = () => {
       </NavLink>
       <ul className={styles.links_list}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about">Sobre</NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Sobre
+          </NavLink>
         </li>
       </ul>
     </nav>
