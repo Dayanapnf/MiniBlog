252dc84473e4f4182aa6c6144f650b344e8a2aff
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-21 11:46:55 -0300

    css

--- MiniBlog/src/pages/Register/Register.jsx
+++ MiniBlog/src/pages/Register/Register.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Register.module.css';
+
 const Register = () => {
   return (
     <div>
-      <h1>Register</h1>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira uma senha"
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme a sua senha"
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
     </div>
   );
 };
