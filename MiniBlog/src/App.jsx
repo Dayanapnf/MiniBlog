c25845f1b023d6405c424c9a5c0f1029d15cfb0f
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-22 06:46:20 -0300

    context

--- MiniBlog/src/App.jsx
+++ MiniBlog/src/App.jsx
@@ -1,26 +1,32 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
+//context
+import { AuthProvider } from './Context/AuthContext';
+//components
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+//pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
