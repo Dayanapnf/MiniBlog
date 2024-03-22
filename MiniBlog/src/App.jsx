db88ab49376a0fa4134aee8156f28cb78d5c0af3
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-22 07:09:10 -0300

    authcontext

--- MiniBlog/src/App.jsx
+++ MiniBlog/src/App.jsx
@@ -1,19 +1,42 @@
 import './App.css';
+
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-//context
-import { AuthProvider } from './Context/AuthContext';
-//components
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-//pages
+import { onAuthStateChanged } from 'firebase/auth';
+
+// hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
+// pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+
+// components
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+// context
+import { AuthProvider } from './Context/AuthContext';
+
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <NavBar />
           <div className="container">
--- MiniBlog/src/Context/AuthContext.jsx
+++ MiniBlog/src/Context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { useContext, createContext } from 'react';
 
 const AuthContext = createContext();
 
-export function AuthProvider({ children }) {
+export function AuthProvider({ children, value }) {
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
