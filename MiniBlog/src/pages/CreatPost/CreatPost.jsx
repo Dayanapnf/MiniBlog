b5c70ed69a8fdb6cc69c98f2356cf29374cbda66
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-31 18:44:53 -0300

    up css

--- MiniBlog/src/pages/CreatPost/CreatPost.jsx
+++ MiniBlog/src/pages/CreatPost/CreatPost.jsx
@@ -73,7 +73,7 @@ const CreatPost = () => {
   };
 
   return (
-    <div className={styles.create_post}>
+    <div className={styles.creat_post}>
       <h2>Criar post</h2>
       <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
       <form onSubmit={handleSubmit}>
