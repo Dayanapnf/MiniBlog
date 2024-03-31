9a8ba189365cb2ace21fa2a405088bb9d53d6a49
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-31 18:19:07 -0300

    up

--- MiniBlog/src/pages/Post/Post.jsx
+++ MiniBlog/src/pages/Post/Post.jsx
@@ -4,9 +4,10 @@ import { useParams } from 'react-router-dom';
 import React from 'react';
 
 const Post = () => {
+  const { id } = useParams();
   return (
     <div>
-      <h1>Post</h1>
+      <h1>Post {id}</h1>
     </div>
   );
 };
