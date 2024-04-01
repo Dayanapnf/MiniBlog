ed8e0ad6f2394623f359aff77f8ff595451ca235
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-04-01 10:21:21 -0300

    dasboard

--- MiniBlog/src/pages/DashBoard/DashBoard.jsx
+++ MiniBlog/src/pages/DashBoard/DashBoard.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import styles from './DashBoard.module.css';
+import { Link } from 'react-router-dom';
+import { useAuthValue } from '../../Context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
 const DashBoard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+  const posts = [];
+  // posts do usuario
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerenciar os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className="{styles.noposts}">
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/creat" className="btn">
+            Criar primeiro Post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   );
 };
